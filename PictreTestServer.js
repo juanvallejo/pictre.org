306e86a9e7fc4fbf615380a7486a59a73fca5328
juanvallejo <juan.vallejo.12@cnu.edu>
2014-01-01 03:13:19 -0500

    fixed restriction on albums when loading from url

--- PictreTestServer.js
+++ PictreTestServer.js
@@ -2,20 +2,26 @@ var fs = require('fs'),PictreTestServer = require('http').createServer(server).l
 
 function server(req,res) {
 	if(req.url.match(/^\/data/gi)) {
+		var Type = {
+			"css":"text/css",
+			"js":"application/javascript",
+			"text":"text/plain"
+		};
 		var file = req.url.split("/");
+		var ext = "text";
 		var path = "";
 		for(var i=0;i<file.length;i++) {
 			var separator = i == file.length-1 ? "" : "/";
 			path+=file[i]+separator;
 		}
-		console.log(path);
 		file = file[file.length-1];
+		ext = file.split("."),ext = ext[ext.length-1];
 		fs.readFile(__dirname+path,function(err,data) {
 			if(err) {
 				res.writeHead(500);
 				return res.end("The file '"+path+"' could not be found.");
 			}
-			res.writeHead(200);
+			res.writeHead(200,{'Content-type':(Type[ext] || Type["text"])});
 			res.end(data);
 		})
 	} else {
